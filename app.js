8af129e35a3a04ba9c5c44243c1b170893e4c6d3
KennethVelazquez <kenny.arcelay@gmail.com>
2025-06-18 23:53:29 -0400

    add JS

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const menuToggle = document.getElementById("menu-toggle");
+const mobileNav = document.getElementById("mobile-nav");
+const dropdowns = document.querySelectorAll(".has-dropdown");
+
+menuToggle.addEventListener("click", () => {
+    mobileNav.classList.toggle("open");
+});
+
+dropdowns.forEach(drop => {
+  const link = drop.querySelector("a");
+
+  link.addEventListener("click", (e) => {
+    e.preventDefault(); // prevent anchor jump
+    drop.classList.toggle("open");
+  });
+});
